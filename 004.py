8948139201a216482737e7ad45e42d46d95fad7a
Polkatt <yangstr@proton.me>
2026-02-26 09:06:24 -0300

    Refactor dice rolling function and input handling

--- 004.py
+++ 004.py
@@ -1,12 +1,29 @@
-import random
+from random import randint 
 
-def rolar_dados(numero):
+def rolagem(lados, bonus=0):
+    valor_dado = randint(1,lados)
+    total = valor_dado + bonus
 
-    valor = random.randint(1,numero)
+    return {
+        "rolagem": valor_dado,
+        "bonus": bonus,
+        "total": total,
+        "texto": f"{valor_dado} + {bonus} = {total}"
+    }
 
-    print(f"Rodando um dado d{numero}")
-    print(f"Resultado: {valor}")
+try:
+    entrada_dado = input("Insira qual dado quer rolar (Ex: d20, d12):\n>>> ").replace("d", "").strip()
+    lados = int(entrada_dado)
 
-numero = int(input("Insira o valor do dado:\n>>> "))
+    tem_bonus = input("Possui algum bonus? (s/n)\n>>> ").lower().strip()
 
-rolar_dados(numero)
+    valor_bonus = 0
+    if tem_bonus.startswith("s"):
+        valor_bonus = int(input("Insira a quantidade do bonus:\n>>> "))
+
+    resultado = rolagem(lados, valor_bonus)
+
+    print(f"\nResultado: {resultado['texto']}")
+
+except ValueError:
+    print("\n [Erro] Por favor, insira apenas números válidos (ex: 20 ou d20).")
